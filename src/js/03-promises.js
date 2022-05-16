d146fa0e5eccc38385a50ef47492d6441b1adbbf
MatveyRubets <rmatvey11@gmail.com>
2022-05-16 17:16:54 +0200

    task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,41 @@
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-const formRef = document.querySelector(".form");
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
-	const shouldResolve = Math.random() > 0.3;
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (shouldResolve) {
-				resolve({ position, delay });
-			} else {
-				reject({ position, delay });
-			}
-		});
-	});
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
 
-function submit(e) {
-	e.preventDefault();
+function submitForm(e) {
+  e.preventDefault();
 
-	let delay = Number(formRef.elements.delay.value);
-	let step = Number(formRef.elements.step.value);
-	let amount = Number(formRef.elements.amount.value);
+  let delay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
 
-	for (let position = 1; position <= amount; position++) {
-		createPromise(position, delay)
-			.then(({ position, delay }) => {
-				setTimeout(() => {
-					Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-				}, delay);
-			})
-			.catch(({ position, delay }) => {
-				Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-			});
-		delay += step;
-	}
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+
+    delay += step;
+  }
 }
 
-formRef.addEventListener("submit", submit);
+form.addEventListener('submit', submitForm);
